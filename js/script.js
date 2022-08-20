25bc8b962c5054a6d94b2e5026bb29c3c52c93a9
siavashsajady <hubgitpro@gmail.com>
2022-08-20 10:29:50 +0200

    Clear save image button from UI

--- js/script.js
+++ js/script.js
@@ -46,6 +46,11 @@ const hideSpinner = () => {
 
 const clearUI = () => {
   qr.innerHTML = '';
+  const saveLink = document.getElementById('save-link');
+
+  if (saveLink) {
+    saveLink.remove();
+  }
 };
 
 const createSaveBtn = (saveUrl) => {
